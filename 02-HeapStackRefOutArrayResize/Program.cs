fd2b1526a0d7f432edcbfb4a177021932f3dd059
Ruad Mammadzada <mammadzada.ruad555@gmail.com>
2026-03-15 15:18:53 +0400

    evtapsirigi

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _02_HeapStackRefOutArrayResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, };
+
+            
+
+           
+            CustomArrResize(ref numbers,   9, 10, 11, 23, 32, 34, 54,45,39);
+
+
+        }
+
+
+        public static void CustomArrResize(ref int[] arr, params int [] numbers2)
+        {
+            int[] newArr = new int[arr.Length + numbers2.Length];
+
+           
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+
+            }
+
+
+            for (int i = 0; i < numbers2.Length; i++)
+            {
+                newArr[arr.Length + i] = numbers2[i];
+            }
+
+
+
+            arr = newArr;
+
+
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                Console.WriteLine(newArr[i]);
+            }
+
+
+
+
+        }
+
+    }
+
+}
