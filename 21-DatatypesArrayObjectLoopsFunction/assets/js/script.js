bfbb0f554c7edd5583d5a1534e1c2c822247500a
Ruad Mammadzada <mammadzada.ruad555@gmail.com>
2026-04-28 22:25:09 +0400

    21 ci task

---
+++ 21-DatatypesArrayObjectLoopsFunction/assets/js/script.js
@@ -0,0 +1,114 @@
+// 1 ci task
+
+// let reqemler = [1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 7, 8, 8, 8, 8];
+
+// let tekrarsizreqemler = [];
+
+// let say = 0;
+
+// for (let i = 0; i < reqemler.length; i++) {
+
+//     if(!tekrarsizreqemler.includes(reqemler[i])){
+
+//         tekrarsizreqemler.push(reqemler[i]);
+//     }
+//     else{
+//         say ++;
+
+//     }
+    
+// }
+
+// console.log("tekrarsiz arry:", tekrarsizreqemler);
+// console.log("tekrarlanan element sayi:", say);
+
+// 2 ci task
+
+// let soz = "apa";
+// let terssoz ="";
+
+// for (let i = soz.length - 1 ; i >=0; i--) {
+
+//     terssoz += soz[i];
+// }
+
+// if(soz === terssoz){
+//     console.log("soz polindromdur");
+// }
+// else{
+//     console.log("soz polindrom deyil");
+// }
+
+// 3 cu task
+
+// let reqemler = [12, 44, 56, 33, 89, 22, 70, 11, 95, 66];
+
+
+// function ededdaxilet(eded){
+
+//     let say = 0;
+
+//     for (let i = 0; i < reqemler.length; i++) {
+//         if(eded < reqemler[i]){
+//             say++;
+//         }
+          
+//     }
+
+//     console.log(`girilen eded ${eded}, ${say} dene elementden kicikdir`)
+// }
+
+// ededdaxilet(56);
+
+// 4 cu task
+
+// function ededyoxla(eded){
+
+//     let cem = 0;
+
+//     for (let i = 1; i <= eded/2 ; i++) {
+//         if(eded % i == 0){
+//             cem += i;
+//         }  
+//     }
+
+//     if(cem > eded){
+//         console.log(`${eded} Aboundant ededdir`);
+//     }
+//     else{
+//         console.log(`${eded} Deficient ededdir`);
+
+//     }
+// }
+
+// ededyoxla(12);
+// ededyoxla(13);
+
+
+
+
+
+// 5 ci task
+
+// let arr = new Array(1,2,3,4,5);
+
+// console.log("cari arry:", arr);
+
+// function kvadratayukseltme(array){
+//     let newarr = [];
+
+// for (let i = 0; i < array.length; i++) {
+//     a = arr[i]**2;
+//     newarr.push(a)
+    
+// }
+
+
+
+// console.log("yeni array:",newarr);
+// }
+
+// kvadratayukseltme(arr);
+
+
+
