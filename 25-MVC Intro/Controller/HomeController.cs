e57c2a8601ab1877cbb80c04261731d444745ee9
Ruad Mammadzada <mammadzada.ruad555@gmail.com>
2026-05-04 21:10:21 +0400

    25.MVC Intro

---
+++ 25-MVC Intro/Controller/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            //return Content("APA 103");
+
+            //var student = new JsonResult(new {id  = 1, name ="ali", surname = "Quliyev"});
+
+            //return student;
+
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return RedirectToAction("index", "Product");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ 25-MVC Intro/Controller/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro
+{
+    public class ProductController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
